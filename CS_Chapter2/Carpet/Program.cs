ed141e2e2e34a5a3835b392d12c7fefadb2eea23
EAHSCS <EAHSCS>
2017-02-06 20:21:48 -0500

    Chapter 2 Work

--- CS_Chapter2/Carpet/Program.cs
+++ CS_Chapter2/Carpet/Program.cs
@@ -9,6 +9,17 @@ namespace Carpet
     {
         static void Main(string[] args)
         {
+            int length = 27;
+            int width = 13;
+            int area = 351;
+            
+
+            Console.WriteLine("Length: " + length + " Width: " + width + " Area: " + length * width );
+            Console.WriteLine("The room is 351 square feet");
+
+
+            Console.WriteLine("Cost: " + area / 2.50);
+            Console.WriteLine("The cost is $" + area / 2.50);
         }
     }
 }
